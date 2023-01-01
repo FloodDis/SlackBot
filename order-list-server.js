10684e5b4969e19b50b7ad844a5a1b26fa2c04b8
Vlad Sechenov <vlad.sechenov@mail.ru>
2023-01-02 05:43:46 +0700

    Added server and html

--- order-list-server.js
+++ order-list-server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const orderList = express();
+const path = require('path');
+const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -8,6 +10,7 @@ orderList.listen(port);
 console.log(`Listening to the port ${port}`);
 
 orderList.set('view engine', 'ejs');
+orderList.set("views", path.join(__dirname, "views"));
 
 orderList.get('/', (req, res) => {
     res.render('order-list');
